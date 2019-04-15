f6ad735e0ebd058f6278c726e25f1f6788493a40
gyq <gao19970120@gmail.com>
2019-04-16 01:39:17 +0800

    update

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import fm1208
 if __name__ == '__main__':
     exam = fm1208.fmos()
     while (1):
-        if input() == 'findcard':
+        inp=input()
+        if inp == 'findcard':
             print(exam.nfcFindCard())
-        if input() == 'selectcard':
+        if inp == 'selectcard':
             print(exam.nfcFindCard())
             print(exam.fmosSelect('3f00'))
 
